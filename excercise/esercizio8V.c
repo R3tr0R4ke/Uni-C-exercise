fc1b09dc1ea2799d5c2326ce4973ac14f2802e17
fede_rake <federico.sgaramella12@gmail.com>
2023-04-19 20:13:04 +0200

    esercizio 8V

---
+++ excercise/esercizio8V.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdbool.h>
+#define MAX_SIZE 10     //dimensione massima dell'array
+
+void main()
+{
+    int vetVoti[MAX_SIZE] = {0};
+    int contaVoti = 0;  //variabile che agisce da indice per il vettore
+
+    bool termina = false;
+    do
+    {
+        int voto;           //variabile dei voti inseriti
+
+        printf("Inserisci un voto: ");
+        scanf("%d", &voto);
+
+        if(voto < 4 || voto > 10)
+        {
+            bool votoInserito = false;
+            do
+            {
+                printf("\nIl voto deve essere un numero compreso tra 4 e 10...\n");
+                printf("Inserisci un voto: ");
+                scanf("%d", &voto);
+
+                if (voto >= 4 && voto <= 10)
+                {
+                    votoInserito = true;
+                }
+            } 
+            while (!votoInserito);
+        }
+
+        vetVoti[contaVoti] = voto;
+        contaVoti ++;
+
+        int scelta;
+        printf("\nVuoi inserire un'altro voto?\n1: SI\n2: NO\nInserisci scelta: ");
+        scanf("%d", &scelta);
+        if (scelta == 1)
+        {
+            puts("");
+            termina = false;
+        }
+        else
+        {
+            puts("");
+            termina = true;
+        }
+    } 
+    while (!termina);
+    
+    printf("Voti: ");
+    for (int i = 0; i < contaVoti; i++)
+    {
+        printf(" %d", vetVoti[i]);
+    }
+
+    float media;
+    int somma = 0;
+    for(size_t i = 0; i < contaVoti; i++)
+    {
+        somma += vetVoti[i];
+    }
+    printf("\nMedia: %.2f", (float)somma / contaVoti);
+}
